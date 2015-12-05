863ee7acfc7461b426ed0b3e62adc2e2c39b6c6e
Patrick <patrick.ester@gmail.com>
2015-12-05 16:38:30 -0600

    Detect Mouse Hits
    Detect if the hammer hits the ruby. Make background flash red for no
    and green for yes. If no click, then no change in background color.

--- whack-a-ruby/whack-a-ruby.rb
+++ whack-a-ruby/whack-a-ruby.rb
@@ -21,6 +21,15 @@ class WhackARuby < Gosu::Window
 			@image.draw(@x - @width / 2, @y - @height / 2, 1)
 		end
 		@hammer_image.draw(mouse_x - 40, mouse_y - 40, 1)
+		if @hit == 0
+			c = Gosu::Color::NONE
+		elseif @hit == 1
+			c = Gosu::Color::GREEN
+		elseif @hit == -1
+			c = Gosu::Color::RED
+		end
+		draw_quad(0, 0, c, 800, 0 , c, 800, 600, c, 0, 600, c)
+		@hit = 0
 	end
 	
 	def update
@@ -30,6 +39,16 @@ class WhackARuby < Gosu::Window
 		@velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
 		@visible -= 1
 		@visible = 30 if @visible < -10 && rand < 0.01
+	end
+	
+	def button_down(id)
+		if (id == Gosu::MsLeft)
+			if Gosu.distance (mouse_x, mouse_y, @x, @y) < 50 && @visisble >= 0
+				@hit = 1
+			else
+				@hit = -1
+			end
+		end
 	end
 end
 
