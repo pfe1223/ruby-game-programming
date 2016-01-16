1f7777f8565120f555379f1e2576a210cc06b527
Patrick <patrick.ester@gmail.com>
2016-01-16 16:50:58 -0600

    Draw and Move
    Add Draw and Move methods to the Bullet class.

--- sector-five/bullet.rb
+++ sector-five/bullet.rb
@@ -1,4 +1,7 @@
 class Bullet
+
+	SPEED = 5
+
 	def initialize(window, x, y, angle)
 		@x = x
 		@y = y
@@ -6,5 +9,14 @@ class Bullet
 		@image = Gosu::Image.new('images/bullet.png')
 		@radius = 3
 		@window = window
+	end
+	
+	def move
+		@x += Gosu.offset_x(@direction, SPEED)
+		@y += Gosu.offset_y(@direction, SPEED)
+	end
+	
+	def draw
+		@image.draw(@x - @radius, @y - @radius, 1)
 	end
 end
