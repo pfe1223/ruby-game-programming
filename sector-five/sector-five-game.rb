d1980617f82880d180d006b767c034daf7fb33e2
Patrick <patrick.ester@gmail.com>
2016-01-17 09:48:50 -0600

    Collision Detection
    Check to see if bullet is hitting an enemy, delete them from the screen.

--- sector-five/sector-five-game.rb
+++ sector-five/sector-five-game.rb
@@ -46,6 +46,15 @@ class SectorFive < Gosu::Window
 		@bullets.each do |bullet|
 			bullet.move
 		end
+		@enemies.dup.each do |enemy|
+			@bullets.dup.each do |bullet|
+				destance = Gosu.distance(enemy.x, enemy.y, bullet.x, bullet.y)
+				if distance < enemy.radius + bullet.radius
+					@enemies.delete enemy
+					@bullets.delete bullet
+				end
+			end
+		end
 	end
 end
 
